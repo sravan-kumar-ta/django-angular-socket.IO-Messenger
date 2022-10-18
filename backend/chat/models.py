9941d07380ae23294fc67278169a6e50659e62ed
sravan-kumar-ta <sravan0095@gmail.com>
2022-10-18 12:42:53 +0530

    database models created

---
+++ backend/chat/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Chat(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    sent_date = models.DateTimeField()
+    message = models.TextField()
